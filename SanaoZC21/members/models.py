16ab194296f85c77ea74be009dbe61241b8615cd
lsanao-eng <lsanao@gbox.adnu.edu.ph>
2026-04-26 18:08:16 -0700

    Create member model with firstname and lastname
    Added member model with firstname and lastname fields.

--- SanaoZC21/members/models.py
+++ SanaoZC21/members/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
+class member(models.Model):
+  firstname = models.Charfield(max_length=255)
+  lastname = models.Charfield(max_length=255)
 # Create your models here.
