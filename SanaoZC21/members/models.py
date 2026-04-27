8372ec450e7e476d66a0c0aa52902ec3da24d5b4
lsanao-eng <lsanao@gbox.adnu.edu.ph>
2026-04-26 19:17:09 -0700

    Update models.py

--- SanaoZC21/members/models.py
+++ SanaoZC21/members/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class member(models.Model):
+class members(models.Model):
   firstname = models.Charfield(max_length=255)
   lastname = models.Charfield(max_length=255)
 # Create your models here.
