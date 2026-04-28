50c51d77187133f390dcc6853355720d25d2a118
lsanao-eng <lsanao@gbox.adnu.edu.ph>
2026-04-27 20:50:36 -0700

    Add files via upload

--- SanaoZC21/members/models.py
+++ SanaoZC21/members/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-class members(models.Model):
-  firstname = models.Charfield(max_length=255)
-  lastname = models.Charfield(max_length=255)
-# Create your models here.
+class Members(models.Model):
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
--- SanaoZC21/members/views.py
+++ SanaoZC21/members/views.py
@@ -1,8 +1,13 @@
 from django.http import HttpResponse
 from django.template import loader
+from .models import Members
 
 def members(request):
-    template = loader.get_template('myfirst.html')
-    return HttpResponse(template.render())
+    mymembers = Members.objects.all().values()
+    template = loader.get_template('index.html')
+    context = {
+        'mymembers': mymembers,
+    }
+    return HttpResponse(template.render(context,request))
 
 # Create your views here.
